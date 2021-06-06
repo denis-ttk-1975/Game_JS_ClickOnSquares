b7cfd6d5ce00c6fdbebff916037264d46af5de34
denis-ttk-1975 <denis.ttk@gmail.com>
2021-06-06 14:17:59 +0300

    random position&size square created

--- index.js
+++ index.js
@@ -1,15 +1,49 @@
 var $start = document.querySelector('#start');
 var $game = document.querySelector('#game');
-console.log ($game);
+var gameFieldSize = $game.getBoundingClientRect();
+
+var score = 0;
 
 $start.addEventListener('click', startGame);
+$game.addEventListener('click', handleBoxClick);
+
 
 function startGame() {
 	$game.style.backgroundColor = '#fff';
+	$game.setAttribute('data-gameField', 'true');
 	$start.classList.add('hide');
 	renderBox();
 }
 
+function handleBoxClick(event) {
+	if(event.target.dataset.box) {
+		renderBox();
+		score++;
+	}
+}
+
 function renderBox () {
+	$game.innerHTML = '';
+	var box = document.createElement('div');
+	var boxSize = getRandom (30, 100);
+	var maxTop = gameFieldSize.height - boxSize;
+	var maxLeft = gameFieldSize.width - boxSize;
+	var boxTop = getRandom (0, maxTop);
+	var boxLeft = getRandom (0, maxLeft);
 	
+
+	box.style.height = box.style.width = boxSize + 'px';
+	box.style.position = 'absolute';
+	box.style.backgroundColor = 'red';
+	box.style.top = boxTop + 'px';
+	box.style.left = boxLeft + 'px';
+	box.style.cursor = 'pointer';
+	box.setAttribute('data-box', 'true');
+
+	$game.insertAdjacentElement('afterbegin', box);
+
+}
+
+function getRandom (min, max) {
+	return Math.floor(Math.random() * (max - min) + min);
 }
